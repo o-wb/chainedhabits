4eca0cfb5740fd339cf671ac8fe9fe743b925dea
o-wb <ollie@websterwebs.com>
2023-11-19 01:37:05 +0300

    added cartesi dapp

--- contract.sol
+++ contract.sol
@@ -1,17 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract MyContract {
-    // State variables
-    
-    // Constructor
-    constructor() {
-        // Initialize state variables
+contract HabitTracker {
+    address private cartesiMachineAddress;
+    mapping(address => string) private userDocuments;
+
+    event DocumentUpdated(address indexed user, string documentHash);
+
+    constructor(address _cartesiMachineAddress) {
+        cartesiMachineAddress = _cartesiMachineAddress;
     }
-    
-    // Modifiers
-    
-    // Events
-    
-    // Functions
+
+    function updateDocument(string memory documentHash) external {
+        require(msg.sender == cartesiMachineAddress, "Only the Cartesi VM can update the document");
+        userDocuments[msg.sender] = documentHash;
+        emit DocumentUpdated(msg.sender, documentHash);
+    }
+
+    function getDocument(address user) external view returns (string memory) {
+        return userDocuments[user];
+    }
 }
