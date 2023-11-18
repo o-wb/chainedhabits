e1a48ea157cf7700cdec34f2974139fe55838af9
o-wb <ollie@websterwebs.com>
2023-11-18 20:24:34 +0300

    demo contract

---
+++ contract.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyContract {
+    // State variables
+    
+    // Constructor
+    constructor() {
+        // Initialize state variables
+    }
+    
+    // Modifiers
+    
+    // Events
+    
+    // Functions
+}
